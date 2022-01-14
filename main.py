70724b9de9f0f86df7f23cc619acc74b6242c0dc
Daniel Bracamontes <59780347+Dedzed101@users.noreply.github.com>
2022-01-14 11:48:47 -0800

    final changes

--- main.py
+++ main.py
@@ -33,76 +33,72 @@ resources = {
 money = 0
 
 
-def check_resource(user_input, given_resource):
-    if given_resource < [MENU][user_input]['ingredients'][given_resource]:
-        return False
-    else:
-        # Deduct resources
-        [MENU][user_input]['ingredients'][given_resource] -= given_resource
+def print_report():
+    """Prints a list of all available resources left in the machine as well as total profit."""
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print("Money: $" + str(money))
+
+
+def resource_sufficient(given_resource):
+    """Function that checks the value of the given resource against the total ingredients list. 
+    Returns False if there is not enough."""
+    for item in given_resource:
+        if given_resource[item] >= resources[item]:
+            print(f"Sorry, not enough {item}.")
+            return False
+    return True
+
+
+def check_coins():
+    """Returns total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("How many Quarters?: ")) * 0.25
+    total += int(input("How many Dimes?: ")) * 0.10
+    total += int(input("How many Nickles?: ")) * 0.05
+    total += int(input("How many Pennies?: ")) * 0.01
+    return total
+
+
+def transaction_successful(money_recieved, drink_cost):
+    """Returns True if enough money inserted, else False."""
+    if money_recieved >= drink_cost:
+        change = round(money_recieved - drink_cost, 2)
+        print(f"Here is your change, ${change} in dollars.")
+        global money
+        money += drink_cost
         return True
-
-
-def check_money(user_input):
-    quarters = input(int("How many quarters?: "))
-    dimes = input(int("How many dimes?: "))
-    nickels = input(int("How many nickels?: "))
-    pennies = input(int("How many pennies?: "))
-
-    money_inserted = (quarters * .25) + (dimes * .10) + \
-        (nickels * .05) + (pennies * .01)
-
-    if money_inserted < MENU[user_input]['cost']:
-        print("Sorry that's not enough money. Money refunded")
-        return False
-
-    elif money_inserted > MENU[user_input]['cost']:
-        change = money_inserted - MENU['espresso']['cost']
-        money += MENU[user_input]['cost']
-        print(f"Here is ${change} dollars in change")
-        return False
     else:
-        money += money_inserted
-        return True
+        print("Sorry, not enough money.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct required ingredients from the resources dictionary."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}!☕️")
 
 
 machine_is_on = True
+
 while machine_is_on:
     user_input = input("What would you like? (espresso/latte/cappuccino?): ")
+
     if user_input == "off":
         machine_is_on = False
+
     if user_input == 'report':
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}g")
-        print("Money: $" + str(money))
+        # A function can be called directly.
+        print_report()
 
-    if user_input == "espresso":
-        if check_resource(user_input, 'water') == False:
-            print("Sorry there is not enough water")
-        if check_resource(user_input, 'coffee') == False:
-            print("Sorry there is not enough coffee")
-
-        elif check_money(user_input) == True:
-            print(f"Here is your {user_input}. Enjoy!")
-
-    if user_input == "latte":
-        if check_resource(user_input, 'water') == False:
-            print("Sorry there is not enough water")
-        if check_resource(user_input, 'milk') == False:
-            print("Sorry there is not enough milk")
-        if check_resource(user_input, 'coffee') == False:
-            print("Sorry there is not enough coffee")
-
-        elif check_money(user_input) == True:
-            print("Here is your {user_input}. Enjoy!")
-
-    if user_input == "cappuccino":
-        if check_resource(user_input, 'water') == False:
-            print("Sorry there is not enough water")
-        if check_resource(user_input, 'milk') == False:
-            print("Sorry there is not enough milk")
-        if check_resource(user_input, 'coffee') == False:
-            print("Sorry there is not enough coffee")
-
-        elif check_money(user_input) == True:
-            print(f"Here is your {user_input}. Enjoy!")
+    else:
+        # Assign variable to the user input that checks the above dictionary. If resource is sufficient
+        # check for sufficient funds.
+        drink = MENU[user_input]
+        if resource_sufficient(drink['ingredients']):
+            # A function call can be called directly (it holds whatever value is defined in the function).
+            payment = check_coins()
+            if transaction_successful(payment, drink['cost']):
+                make_coffee(user_input, drink['ingredients'])
