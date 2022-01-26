c321e8c5adc62aa58b28c8ffcbb45bcd69a57f2c
Daniel Bracamontes <59780347+Dedzed101@users.noreply.github.com>
2022-01-25 18:53:35 -0800

    Day 17 - Quiz Game

--- Day 17 - Quiz Game/main.py
+++ Day 17 - Quiz Game/main.py
@@ -11,4 +11,9 @@ for question in question_data:
     question_bank.append(new_question_obj)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.player_score}/12")
+print('\n')
