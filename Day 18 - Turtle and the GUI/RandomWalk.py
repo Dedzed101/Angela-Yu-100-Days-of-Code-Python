a1646fb2c2cfbc98246c6fd33eca126c3325437a
Daniel Bracamontes <59780347+Dedzed101@users.noreply.github.com>
2022-01-31 23:19:07 -0800

    ok

--- Day 18 - Turtle and the GUI/RandomWalk.py
+++ Day 18 - Turtle and the GUI/RandomWalk.py
@@ -2,17 +2,36 @@ from turtle import *
 import random
 
 tt = Turtle()
-tt.shape()
+tt.shape('turtle')
+tt.pensize(7)
+tt.speed('slow')
 
 colors = ['red', 'orange', 'gold1', 'green',
           'blue', 'indigo', 'violet', 'black']
+directions = [0, 90, 180, 270]
 
-while True:
 
-    # set turtle to move forward a radnom number fo steps
-    # set turtle to turn either right or left (at random) a radnom number fo degrees and move forward
-    # with a random color
-tt.color(random.choice(colors))
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
+for i in range(200):
+    tt.color(random_color())
+    tt.forward(30)
+    tt.setheading(random.choice(directions))
+
+# while True:
+#     # with a random color
+#     tt.color(random_color())
+#     # set turtle to move forward a radnom number fo steps
+#     # tt.forward(30) for hardcoded distance
+#     tt.forward(random.randint(1, 100))
+#     # set turtle to turn either right or left (at random) a radnom number fo degrees and move forward
+#     tt.setheading(random.choice(directions))
 
 screen = Screen()
 screen.exitonclick()
--- Day 18 - Turtle and the GUI/main.py
+++ Day 18 - Turtle and the GUI/main.py
@@ -1,8 +1,28 @@
 from turtle import *
 import random
+
 # tt for Timmy the Turtle
 tt = Turtle()
 tt.shape()
+
+colors = ['red', 'orange', 'gold1', 'green',
+          'blue', 'indigo', 'violet', 'black']
+
+
+def short_function(num_sides):
+    angle = 360 / num_sides
+    for i in range(num_sides):
+        tt.forward(100)
+        tt.right(angle)
+
+
+for shape_side_n in range(3, 11):
+    tt.color(random.choice(colors))
+    short_function(shape_side_n)
+
+# Screen must happen at the end, after we have programmed Turtle
+screen = Screen()
+screen.exitonclick()
 
 # for i in range(20):
 #     tt.pencolor("red")
@@ -44,24 +64,3 @@ tt.shape()
 #     tt.pencolor('black')
 #     tt.forward(100)
 #     tt.right(36)
-
-
-colors = ['red', 'orange', 'gold1', 'green',
-          'blue', 'indigo', 'violet', 'black']
-
-
-def short_function(num_sides):
-    angle = 360 / num_sides
-    for i in range(num_sides):
-        tt.forward(100)
-        tt.right(angle)
-
-
-for shape_side_n in range(3, 11):
-    tt.color(random.choice(colors))
-    short_function(shape_side_n)
-
-
-# Screen must happen at the end, after we have programmed Turtle
-screen = Screen()
-screen.exitonclick()
